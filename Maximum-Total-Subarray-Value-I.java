83f645be17d397bd8dccea71f741234ca140a110
RahulKumar262 <ry818568@gmail.com>
2026-06-09 22:20:06 +0530

    [3689] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ Maximum-Total-Subarray-Value-I.java
@@ -0,0 +1,10 @@
+1class Solution {
+2    public long maxTotalValue(int[] nums, int k) {
+3        int max=nums[0], min=nums[0];
+4        for(int num : nums){
+5            max=Math.max(max,num);
+6            min=Math.min(min,num);
+7        }
+8        return (long)k*(max-min);
+9    }
+10}
